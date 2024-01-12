a93b42e8e1d477115a95c4f12a67350b4c42f2d5
Daniela Hengl <d.hengl@icloud.com>
2024-01-12 11:13:40 +0100

    -removed Client target as it conflicts due to UIKit import

--- Package.swift
+++ Package.swift
@@ -14,20 +14,13 @@ let package = Package(
     .library(
       name: name,
       targets: [name]
-    ),
-    .executable(
-      name: "\(name)Client",
-      targets: ["\(name)Client"]
     )
   ],
   dependencies: [
-    .package(url: "git@github.com:num42/swift-macrotester.git", from: "1.0.0"),
+    .package(url: "git@github.com:num42/swift-macrotester.git", from:"1.0.0"),
     // Depend on the Swift 5.9 release of SwiftSyntax
     .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
-    .package(
-      url: "https://github.com/realm/SwiftLint",
-      from: "0.53.0"
-    )
+    .package(url: "https://github.com/realm/SwiftLint", from: "0.54.0")
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -41,26 +34,18 @@ let package = Package(
       ],
       plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]
     ),
-
     // Library that exposes a macro as part of its API, which is used in client programs.
     .target(
       name: name,
       dependencies: [.target(name: "\(name)Macros")],
       plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]
     ),
-
-    // A client of the library, which is able to use the macro in its own code.
-    .executableTarget(
-      name: "\(name)Client",
-      dependencies: [.target(name: name)],
-      plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]
-    ),
     // A test target used to develop the macro implementation.
     .testTarget(
       name: "\(name)Tests",
       dependencies: [
-        .product(name: "MacroTester", package: "MacroTester"),
         .target(name: "\(name)Macros"),
+        .product(name: "MacroTester", package: "swift-macrotester"),
         .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax")
       ],
       plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]
